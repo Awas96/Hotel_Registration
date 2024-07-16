a8789d390accc12d02d45dc7815617dbc473dc63
Jose Antonio Roselló López <jrosellolopez857@gmail.com>
2024-07-16 02:16:20 +0200

    Implemented LexitJWTBundle and mada changes to implement jwt login

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
